c101341a9984726339c369dea559b6380578ad7d
alexander <lokistapway@yandex.ru>
2023-06-04 20:46:23 +0300

    Сделан отдельный компонент для текущей погоды города

---
+++ src/components/CurrentWeatherCity.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="weather-sheet-current">
+    <p class="weather-sheet-current__city">
+      {{ cityWeatherData?.location?.name }}
+    </p>
+    <p class="weather-sheet-current__date">
+      Сейчас {{ cityWeatherData?.location?.localtime }}
+    </p>
+    <p class="weather-sheet-current__temperature">
+      {{ cityWeatherData?.current?.temp_c }}
+    </p>
+    <p class="weather-sheet-current__wind-speed">
+      {{ cityWeatherData?.current?.wind_kph }}
+    </p>
+    <p class="weather-sheet-current__humidity">
+      {{ cityWeatherData?.current?.humidity }}
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+import {mapActions, mapState} from "vuex";
+
+export default defineComponent({
+  name: 'CityCurrentWeather',
+
+  computed: {
+    ...mapState([
+        'cityWeatherData',
+    ]),
+  },
+
+  methods: {
+    ...mapActions([
+      'getCityWeather',
+    ]),
+  },
+
+  created() {
+    this.getCityWeather();
+  },
+})
+</script>
+
+<style scoped>
+  .weather-sheet-current{
+    display: grid;
+    grid-template-columns: repeat(4, auto) 1fr;
+    column-gap: 20px;
+  }
+
+  .weather-sheet-current__city,
+  .weather-sheet-current__date,
+  .weather-sheet-current__temperature{
+    grid-column: 1/6;
+  }
+</style>
--- src/components/WeatherSheet.vue
+++ src/components/WeatherSheet.vue
@@ -1,46 +1,17 @@
 <template>
   <div class="weather-sheet">
-    <div class="weather-sheet-current">
-      <p class="weather-sheet-current__city">
-        {{ cityWeatherData?.location?.name }}
-      </p>
-      <p class="weather-sheet-current__date">
-        Сейчас {{ cityWeatherData?.location?.localtime }}
-      </p>
-      <p class="weather-sheet-current__temperature">
-        {{ cityWeatherData?.current?.temp_c }}
-      </p>
-      <p class="weather-sheet-current__wind-speed">
-        {{ cityWeatherData?.current?.wind_kph }}
-      </p>
-      <p class="weather-sheet-current__humidity">
-        {{ cityWeatherData?.current?.humidity }}
-      </p>
-    </div>
+    <CurrentWeatherCity />
   </div>
 </template>
 
 <script lang="ts">
 import {defineComponent} from "vue";
-import {mapActions, mapState} from "vuex";
+import CurrentWeatherCity from "@/components/CurrentWeatherCity.vue";
 
 export default defineComponent({
   name: 'WeatherSheet',
-
-  computed: {
-    ...mapState([
-        'cityWeatherData',
-    ]),
-  },
-
-  methods: {
-    ...mapActions([
-        'getCityWeather',
-    ]),
-  },
-
-  created() {
-    this.getCityWeather();
+  components: {
+    CurrentWeatherCity,
   },
 })
 </script>
@@ -53,17 +24,5 @@ export default defineComponent({
     background: #03a9f4;
     border-radius: 8px;
     padding: 20px;
-  }
-
-  .weather-sheet-current{
-    display: grid;
-    grid-template-columns: repeat(4, auto) 1fr;
-    column-gap: 20px;
-  }
-
-  .weather-sheet-current__city,
-  .weather-sheet-current__date,
-  .weather-sheet-current__temperature{
-    grid-column: 1/6;
   }
 </style>
