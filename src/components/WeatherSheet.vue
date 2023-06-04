152b20933a3ea3bc8b360e448d75988526e0391a
alexander <lokistapway@yandex.ru>
2023-06-04 20:35:13 +0300

    Данные с текущей погодой перенесены в стор

--- src/components/WeatherSheet.vue
+++ src/components/WeatherSheet.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="weather-sheet">
-    <div class="weather-sheet-day">
-      <p class="weather-sheet-day__date">
+    <div class="weather-sheet-current">
+      <p class="weather-sheet-current__city">
         {{ cityWeatherData?.location?.name }}
       </p>
-      <p class="weather-sheet-day__temperature">
+      <p class="weather-sheet-current__date">
+        Сейчас {{ cityWeatherData?.location?.localtime }}
+      </p>
+      <p class="weather-sheet-current__temperature">
         {{ cityWeatherData?.current?.temp_c }}
+      </p>
+      <p class="weather-sheet-current__wind-speed">
+        {{ cityWeatherData?.current?.wind_kph }}
+      </p>
+      <p class="weather-sheet-current__humidity">
+        {{ cityWeatherData?.current?.humidity }}
       </p>
     </div>
   </div>
@@ -13,16 +22,15 @@
 
 <script lang="ts">
 import {defineComponent} from "vue";
-import {mapActions} from "vuex";
-import CityWeather from "@/models/CityWeather";
+import {mapActions, mapState} from "vuex";
 
 export default defineComponent({
   name: 'WeatherSheet',
 
-  data() {
-    return {
-      cityWeatherData: {} as CityWeather,
-    }
+  computed: {
+    ...mapState([
+        'cityWeatherData',
+    ]),
   },
 
   methods: {
@@ -31,8 +39,8 @@ export default defineComponent({
     ]),
   },
 
-  async created() {
-    this.cityWeatherData = await this.getCityWeather();
+  created() {
+    this.getCityWeather();
   },
 })
 </script>
@@ -40,10 +48,22 @@ export default defineComponent({
 <style scoped>
   .weather-sheet{
     display: grid;
-    grid-template-columns: 1fr 1fr;
+    grid-template-columns: 1fr;
     justify-content: center;
     background: #03a9f4;
     border-radius: 8px;
     padding: 20px;
+  }
+
+  .weather-sheet-current{
+    display: grid;
+    grid-template-columns: repeat(4, auto) 1fr;
+    column-gap: 20px;
+  }
+
+  .weather-sheet-current__city,
+  .weather-sheet-current__date,
+  .weather-sheet-current__temperature{
+    grid-column: 1/6;
   }
 </style>
--- src/store/index.ts
+++ src/store/index.ts
@@ -3,12 +3,18 @@ import api from "@/api/api";
 import CityWeather from "@/models/CityWeather";
 
 export default createStore({
-    state: {},
+    state: {
+        cityWeatherData: {} as CityWeather,
+    },
     getters: {},
-    mutations: {},
+    mutations: {
+        setCityWeatherData(state, data: CityWeather) {
+            state.cityWeatherData = data;
+        },
+    },
     actions: {
-        async getCityWeather(): Promise<CityWeather> {
-            return await api.getCityWeather();
+        async getCityWeather({ commit }) {
+            commit('setCityWeatherData', await api.getCityWeather());
         },
     },
 });
