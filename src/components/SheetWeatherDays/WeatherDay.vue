ef8139a19daee0f82c54c15a5b9a3f3e3f22b61b
alexander <lokistapway@yandex.ru>
2023-06-10 11:15:07 +0300

    Добавлено форматирование температуры

--- src/components/SheetWeatherDays/WeatherDay.vue
+++ src/components/SheetWeatherDays/WeatherDay.vue
@@ -7,7 +7,7 @@
       {{ getDate }}
     </p>
     <p class="weather-day__temp">
-      {{ weatherData.day.maxtemp_c }}
+      {{ getTemp }}
     </p>
   </li>
 </template>
@@ -26,8 +26,8 @@ export default defineComponent({
   },
 
   computed: {
-    getWeek() {
-      const weekDays = [
+    getWeek(): string {
+      const weekDays: string[] = [
           'Пн',
           'Вт',
           'Ср',
@@ -40,8 +40,8 @@ export default defineComponent({
       return weekDays[new Date(this.weatherData.date).getDay()];
     },
 
-    getDate() {
-      const months = [
+    getDate(): string {
+      const months: string[] = [
           'января',
           'февраля',
           'марта',
@@ -60,6 +60,12 @@ export default defineComponent({
 
       return currentDate.getDate() + ' ' + months[currentDate.getMonth()];
     },
+
+    getTemp(): string {
+      const temperature = this.weatherData.day.maxtemp_c;
+      const plus: string = (temperature > 0) ? '+' : '';
+      return plus + '' + Math.round(temperature);
+    },
   },
 })
 </script>
@@ -71,10 +77,29 @@ export default defineComponent({
     box-shadow: rgb(0 0 0 / 35%) 0px 5px 15px;
     background: #03a9f4;
     color: #fff;
+    display: flex;
+    flex-direction: column;
+    align-items: start;
   }
 
   .weather-day__week{
     font-weight: bold;
     text-transform: uppercase;
+  }
+
+  .weather-day__temp{
+    position: relative;
+    font-weight: bold;
+  }
+
+  .weather-day__temp:after{
+    position: absolute;
+    content: '';
+    top: 2px;
+    right: -10px;
+    width: 8px;
+    height: 8px;
+    border-radius: 50%;
+    border: 2px solid #fff;
   }
 </style>
