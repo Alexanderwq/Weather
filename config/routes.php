aa88e11b7d926be03190d6d5cc23e85f8c740a9a
alexander-pc <lokistapway@yandex.ru>
2023-06-11 22:56:08 +0300

    Сделан вывод vue на страницу

--- config/routes.php
+++ config/routes.php
@@ -6,7 +6,7 @@ use Slim\App;
 
 return function (App $app) {
     $app->get('/', function (ServerRequestInterface $request, ResponseInterface $response) {
-        $response->getBody()->write("<div>main page</div>");
+        $response->getBody()->write(file_get_contents('vue/dist/index.html'));
 
         return $response;
     });
