25d5b5e2ca139a490beea03b6e7ebc0ee2a6f538
alexander <lokistapway@yandex.ru>
2023-06-17 19:30:33 +0300

    Создан маршрут для получения данных о текущей погоде

--- config/routes.php
+++ config/routes.php
@@ -1,8 +1,11 @@
 <?php
 
+use Api\Action\GetCurrentWeatherAction;
 use App\Action\HomeAction;
 use Slim\App;
 
 return function (App $app) {
     $app->get('/', HomeAction::class);
+
+    $app->post('/get_current_weather', GetCurrentWeatherAction::class);
 };
