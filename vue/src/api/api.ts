ebac132447a6744219eca8732a69b0202c90ec52
alexander <lokistapway@yandex.ru>
2023-06-17 20:26:29 +0300

    Поправлен запрос для получения данных о текущей погоде

--- vue/src/api/api.ts
+++ vue/src/api/api.ts
@@ -1,15 +1,17 @@
 import axios from "axios";
-import { url, apiKey } from "./consts";
 import CityWeather from "../models/CityWeather";
 
 export default {
     getCityWeather(): Promise<CityWeather> {
-        return axios.get(url + '/forecast.json', {
-            params: {
-                key: apiKey,
-                q: 'Ярославль',
-                days: 10,
-            },
-        }).then(res => res.data);
+        return axios.post('/get_current_weather',
+            JSON.stringify({
+                    city: 'Ярославль',
+            }),
+            {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        ).then(res => res.data);
     },
 }
