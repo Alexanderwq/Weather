cf9d47f42544019447d92748a646bc9ce89d2858
alexander <lokistapway@yandex.ru>
2023-07-15 22:14:31 +0300

    Добавлена тень к основным блокам

--- vue/src/components/SheetWeatherDays/SheetWeatherDays.vue
+++ vue/src/components/SheetWeatherDays/SheetWeatherDays.vue
@@ -28,6 +28,8 @@ export default defineComponent({
     display: flex;
     flex-direction: column;
     gap: 15px;
+    border: rgba(10,37,64,0.1);
+    box-shadow: 0 0 2px 0 rgba(0,8,50,0.04),0 2px 4px 0 rgba(0,8,50,0.04),0 3px 12px 1px rgba(0,8,50,0.08);
   }
 
   .sheet-days__title{
--- vue/src/components/WeatherSheet/WeatherSheet.vue
+++ vue/src/components/WeatherSheet/WeatherSheet.vue
@@ -27,5 +27,7 @@ export default defineComponent({
     background: #03a9f4;
     border-radius: 8px;
     padding: 20px;
+    border: rgba(10,37,64,0.1);
+    box-shadow: 0 0 2px 0 rgba(0,8,50,0.04),0 2px 4px 0 rgba(0,8,50,0.04),0 3px 12px 1px rgba(0,8,50,0.08);
   }
 </style>
